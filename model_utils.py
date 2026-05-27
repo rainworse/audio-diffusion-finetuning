66093b39a960101652a2126b16373769c24c1cc5
Gustas Mikelionis <mikelionisgustas@gmail.com>
2026-05-27 22:06:11 +0100

    refactor

---
+++ model_utils.py
@@ -0,0 +1,63 @@
+import torch
+from transformers import AutoModel, AutoTokenizer
+import soundfile as sf
+import torchaudio
+from pathlib import Path
+from stable_audio_vae import StableAudioVAE
+from huggingface_hub import hf_hub_download
+
+def load_transformer_model(model_repo, model_dtype, device):
+    model = AutoModel.from_pretrained(model_repo, trust_remote_code=True,
+                                      attn_implementation="sdpa", dtype=model_dtype).to(device).to(model_dtype)
+    model.tokenizer.quantizer.project_out.register_forward_pre_hook(cast_input_hook)
+    return model
+
+def cast_input_hook(module, args):
+    return tuple(a.to(module.weight.dtype) if isinstance(a, torch.Tensor) else a for a in args)
+
+def load_prompt_embedding_model_and_tokenizer(llm_repo_id, model_dtype, device):
+    prompt_embedding_model = AutoModel.from_pretrained(llm_repo_id, dtype=model_dtype).to(device).to(model_dtype)
+    tokenizer = AutoTokenizer.from_pretrained(llm_repo_id)
+    return prompt_embedding_model, tokenizer
+
+def get_reference_audio_latent(encoder, device, model_dtype, silence_latent, use_reference=True):
+    ref_path = "inputs/reference1.wav"
+    if use_reference and Path(ref_path).exists():
+        MAX_SAMPLES = 48000 * 60
+        audio, sr = sf.read(ref_path, always_2d=True)
+        wav = torch.from_numpy(audio.T).float().to(device).to(model_dtype)
+        wav = wav[..., :MAX_SAMPLES]
+
+        if sr != 48000:
+            wav = torchaudio.functional.resample(wav, orig_freq=sr, new_freq=48000)
+
+        print(wav.shape)
+        reference_latent = encoder.encode(wav).to(device).to(model_dtype)
+        reference_latent = reference_latent.permute(0, 2, 1)
+        mask = torch.LongTensor([0]).to(device)
+        return reference_latent, mask
+    print("No reference audio")
+    return silence_latent.permute(0, 2, 1), torch.LongTensor([0]).to(device)
+
+def load_encoder(config_path, checkpoint_path, device, model_dtype):
+    return StableAudioVAE(config_path=config_path,checkpoint_path=checkpoint_path).to(device).to(model_dtype)
+
+def load_silence_latent(model_repo, filename, device, model_dtype):
+    path = hf_hub_download(repo_id=model_repo, filename=filename)
+    silence_latent = torch.load(path).to(device).to(model_dtype)
+    return silence_latent
+
+def save_audio(audio_data, sample_rate=48000):
+
+    import soundfile as sf
+
+    output_path = Path("./outputs/output.wav")
+    audio_np = audio_data.to(torch.float32).cpu().detach().numpy().T.reshape((-1, 2))
+    sf.write(output_path, audio_np, sample_rate)
+    return audio_np
+
+def decode_latent_and_save_audio(latents, decoder, sample_rate=48000):
+    output = decoder.decode(latents)
+    print(output.shape)
+    audio_np = save_audio(output, sample_rate)
+    print(audio_np.shape)
