626699bb8fdacde726dd41e0d3e89a16a256c2d7
Gustas Mikelionis <gustas.mikelionis@siemens.com>
2026-05-30 12:14:22 +0100

    dependency changes to account for lower cuda versions, minor refactor

--- model_utils.py
+++ model_utils.py
@@ -1,10 +1,7 @@
 import torch
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel
 import soundfile as sf
-import torchaudio
 from pathlib import Path
-from stable_audio_vae import StableAudioVAE
-from huggingface_hub import hf_hub_download
 
 def load_transformer_model(model_repo, model_dtype, device):
     model = AutoModel.from_pretrained(model_repo, trust_remote_code=True,
@@ -16,11 +13,14 @@ def cast_input_hook(module, args):
     return tuple(a.to(module.weight.dtype) if isinstance(a, torch.Tensor) else a for a in args)
 
 def load_prompt_embedding_model_and_tokenizer(llm_repo_id, model_dtype, device):
+    from transformers import AutoTokenizer
     prompt_embedding_model = AutoModel.from_pretrained(llm_repo_id, dtype=model_dtype).to(device).to(model_dtype)
     tokenizer = AutoTokenizer.from_pretrained(llm_repo_id)
     return prompt_embedding_model, tokenizer
 
 def load_audio(file_path, device, model_dtype):
+    import torchaudio
+
     MAX_SAMPLES = 48000 * 60
     audio, sr = sf.read(file_path, always_2d=True)
     wav = torch.from_numpy(audio.T).float().to(device).to(model_dtype)
@@ -43,7 +43,7 @@ def get_reference_audio_latent(encoder, device, model_dtype, silence_latent, use
     print("No reference audio")
     return silence_latent.permute(0, 2, 1), torch.LongTensor([0]).to(device)
 
-def load_finetuning_audio_latents(encoder, device, model_dtype):
+def load_finetuning_audio_latents(encoder, device, model_dtype, lim=-1):
     path = "inputs/lora"
     wavs = []
 
@@ -51,22 +51,26 @@ def load_finetuning_audio_latents(encoder, device, model_dtype):
         if file.is_file() and file.suffix == ".wav":
             audio_latent = load_audio(file, device, model_dtype)
             wavs.append(audio_latent)
+    if lim != -1:
+        wavs = wavs[:lim]
     wavs = torch.stack(wavs).to(device).to(model_dtype)
     print(wavs.shape)
     latents = encoder.encode(wavs).to(device).to(model_dtype)
     return latents
 
 def load_encoder(config_path, checkpoint_path, device, model_dtype):
+    from stable_audio_vae import StableAudioVAE
+
     return StableAudioVAE(config_path=config_path,checkpoint_path=checkpoint_path).to(device).to(model_dtype)
 
 def load_silence_latent(model_repo, filename, device, model_dtype):
+    from huggingface_hub import hf_hub_download
+
     path = hf_hub_download(repo_id=model_repo, filename=filename)
     silence_latent = torch.load(path).to(device).to(model_dtype)
     return silence_latent
 
 def save_audio(audio_data, filename, sample_rate=48000):
-
-    import soundfile as sf
 
     output_path = Path("./outputs/" + filename)
     audio_np = audio_data.to(torch.float32).cpu().detach().numpy().T.reshape((-1, 2))
